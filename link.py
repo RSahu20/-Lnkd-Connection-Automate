94085c6c86d0ad597c96b28992a89032f8ed542f
Raj Sahu <80415025+RSahu20@users.noreply.github.com>
2022-05-19 20:00:50 +0530

    Add files via upload

---
+++ link.py
@@ -0,0 +1,51 @@
+# connect with webbrowser-chrome
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import pyautogui as pag
+
+
+def login():
+
+	# login element
+	username = driver.find_element_by_id("login-email")
+
+	# keys for username	
+	username.send_keys("username")
+
+	# Getting the password 							
+	password = driver.find_element_by_id("login-password")
+
+	# keys for password
+	password.send_keys("password")	
+
+	# tag for submit button					
+	driver.find_element_by_id("login-submit").click()		
+
+def goto_network():
+	driver.find_element_by_id("mynetwork-tab-icon").click()
+
+def send_requests():
+
+	# requests you want to send
+	n = input("Number of requests: ")
+
+	for i in range(0, n):
+		# position(in px) of connection button
+		pag.click(880, 770)
+	print("Done !")
+
+def main():
+
+	# url of LinkedIn
+	url = "http://linkedin.com/"
+
+	# url of LinkedIn network page
+	network_url = "http://linkedin.com / mynetwork/"
+
+	# path to browser web driver
+	driver = webdriver.Chrome('C:\\Program Files\\Web Driver\\chromedriver.exe')	
+	driver.get(url)
+
+# Driver's code
+if __name__ == __main__:
+	main()
